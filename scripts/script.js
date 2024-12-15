7ce753628b6e3d59d695a68a6feb98d9f1ad41f2
robertomorano <roberto.morano@iesnervion.es>
2024-12-15 16:39:53 +0100

    single operation

--- scripts/script.js
+++ scripts/script.js
@@ -2,39 +2,52 @@ let num1 = null;
 let num2 = null;
 let result= null;
 let operator = null;
-let cont = 0;
 let checkOperator = false;
 const buttons = document.querySelectorAll(".btn");
 const display = document.querySelector(".display");
 const clickEvent = (event) => {
     //console.log(event);
    if (event.target.className === "btn operator"){
-    if(!checkOperator){
-        operator = event.target.id;
-        //Equals to number 2 if you click 2 times
-        num2  = num1;
-        checkOperator = true;
-    }else{
-        //Execute function equal if you click 2 times an operator
+      //if (number){  
+        if(event.target.id !== operator){
+            operator = event.target.id;
+            //Equals to number 2 if you click 2 times
+            checkOperator = true;
+        }else{
+            num2=num1;
+            //Execute function equal if you click 2 times an operator
+            operate(num1,num2,operator);
+            clear();
+       // }
+    }
+   }else if (event.target.className === "btn equal"){
+    if(checkOperator){
+        operate(num1,num2,operator);
+        clear();
         
     }
-   }else if (event.target.className === "btn"){
+   }else if (event.target.className === "btn dot"){
+
+   }else if(event.target.className === "btn backspace"){  
+    //if(checkOperator)
+   }else{
     setNumber(event.target.id, checkOperator);
    }
 };
 buttons.forEach((button) => {
-    // and for each one we add a 'click' listener
+    //for each one we add a 'click' listener
     button.addEventListener("click", clickEvent);
 });
 function setNumber(id, checkOperator){
     //Check if is the first number or the second
     console.log(id);
     if (!checkOperator){
+        
         num1 = (num1*10)+parseInt(id);
-        cont++;
         displayNumber(num1);
     }else {
-
+        num2 = (num2*10)+parseInt(id);
+        displayNumber(num2);
     }
 }
 
@@ -43,7 +56,6 @@ function displayNumber(numToDysplay){
 }
 
 function operate(num1,num2,operator){
-    
     switch (operator) {
         case "+":
             result = num1+num2;
@@ -61,4 +73,12 @@ function operate(num1,num2,operator){
             break;
     }
     displayNumber(result);
+    result=0;
+}
+function clear(){
+    num1= null;
+    num2 = null;
+    result= null;
+    operator = "";
+    checkOperator = false;
 }
